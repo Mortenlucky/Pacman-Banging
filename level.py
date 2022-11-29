8549087a247f575e457acbc62768f634f880a042
Clara <clar.holst@gmail.com>
2022-11-29 14:54:29 +0100

    points

--- level.py
+++ level.py
@@ -20,3 +20,13 @@ class Level:
                 if tile == "#":
                     pg.draw.rect(screen, (10,10,250), pg.Rect(col_idx*32+1, row_idx*32+1, 30, 30), 1)
 
+    def draw(self, screen, pacman):
+        for row_idx, row in enumerate(self.tiles):
+            for col_idx, tile in enumerate(row):
+                if tile == "#":
+                    pg.draw.rect(screen, (10,10,250), pg.Rect(col_idx*32+1, row_idx*32+1, 30, 30), 1)
+                elif tile == " ":
+                    pg.draw.circle(screen,(220,220,0), (col_idx*32+16, row_idx*32+16),5)
+        font = pg.font.SysFont("arial", 32)
+        score = font.render(f"Points: {pacman.point}", True, (255,255,255))
+        screen.blit(score, (5,5))
